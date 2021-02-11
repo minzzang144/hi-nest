4203b66c4d002d5b22f076db47bbf51ca97257bc
MinChan Lee <shigatsu970704@gmail.com>
2021-02-11 14:16:31 +0900

    01.Controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,15 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @는 데코레이터로 그냥 함수라고 생각하면 편리할 것 같다.
+  // @Get은 Express에서 라우터에 해당되는 app.get메서드와 아주 비슷하며 getHello는 그에 관한 컨트롤 함수라고 생각할 수 있다.
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hellow')
+  sayHello(): string {
+    return 'Hellow everyone';
   }
 }
