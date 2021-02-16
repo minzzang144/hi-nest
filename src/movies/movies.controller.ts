e6756fc44ee771739cd8060273f712510b5a8f3f
MinChan Lee <shigatsu970704@gmail.com>
2021-02-16 15:06:12 +0900

    14.Testing Patch ad Delete Movie Id(E2E)

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -23,7 +23,6 @@ export class MoviesController {
 
   @Get(':id')
   getOne(@Param('id') movieId: number): Movie {
-    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -30,10 +30,10 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('GET', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -43,16 +43,34 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
+    it('POST 404', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2020,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
 
   describe('/movies/:id', () => {
-    it('GET 404', () => {
-      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
